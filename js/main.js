49bb5f56e441cacde8ca364d7aea701739721953
filippo199600 <pippotartaglia@gmail.com>
2023-08-28 21:54:55 +0200

    feat milestone1

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-// array di immagini
-const images = [
-  {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-  },
-  {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-  },
-  {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  },
-  {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-  },
-  {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
-
 // // collego i miei elementi del dom
 // const jumbo = document.getElementById("jumbo");
 // const arrowRight = document.getElementById("arrow-right");
@@ -73,3 +44,63 @@ const images = [
 //     counter = 0;
 //   }
 // });
+
+// array di immagini
+const images = [
+  {
+    image: "img/01.webp",
+    title: "Marvel's Spiderman Miles Morale",
+    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+  },
+  {
+    image: "img/02.webp",
+    title: "Ratchet & Clank: Rift Apart",
+    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+  },
+  {
+    image: "img/03.webp",
+    title: "Fortnite",
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  },
+  {
+    image: "img/04.webp",
+    title: "Stray",
+    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+  },
+  {
+    image: "img/05.webp",
+    title: "Marvel's Avengers",
+    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+  },
+];
+
+// elemnti del dom
+const containerImmagini = document.getElementById("container-immagini");
+
+// variabile che attiva le immagini
+let immagineAttiva = 0;
+
+// ciclo infinito del carosello
+images.forEach((immagine, indice) => {
+  // creo il nodoHTML
+  const immagineElemento = document.createElement("div");
+  immagineElemento.classList.add("immagine");
+
+  if (indice == immagineAttiva)
+    immagineElemento.classList.add("immagine-attiva");
+
+  immagineElemento.innerHTML = `
+  <img src="./${immagine.image}" class="" alt="" />
+
+  <div class="presentazione">
+    <h2>${immagine.title}</h2>
+    <p>${immagine.text}</p>
+  </div>`;
+
+  // aggiungo all'array il nodoHTML
+  immagine.nodoHTML = immagineElemento;
+
+  // appendo il nodo al container
+  containerImmagini.append(immagineElemento);
+});
+console.log(images);
